174c9bb91cf1f6fd0a42ea40df9082f7ed8a36bc
Zidan Khulul Sajid <zidanreborn@gmail.com>
2023-02-22 10:14:37 +0700

    add create and delete letters with error and success handler

--- app/Http/Controllers/Backoffice/DashboardController.php
+++ app/Http/Controllers/Backoffice/DashboardController.php
@@ -12,11 +12,11 @@ class DashboardController extends Controller
     public function index() {
         try
         {
-            $resident = Resident::all()->count();
-            $letter = Letter::all()->count();
+            // $resident = Resident::all()->count();
+            // $letter = Letter::all()->count();
             return inertia::render('Dashboard', [
-                'resident' => $resident,
-                'letter'   => $letter
+                // 'resident' => $resident,
+                // 'letter'   => $letter
             ], 200);
         } catch(\Throwable $th) {
             return response()->json([
